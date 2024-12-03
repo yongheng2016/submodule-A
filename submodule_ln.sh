fefbb2a9c19f38069fbbd3d6a78f32291183a9c4
yyh <24493052+yongheng2016@users.noreply.github.com>
2024-12-03 19:32:44 +0800

    fix: update

--- submodule_ln.sh
+++ submodule_ln.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 
 CURRENT_DIR=$(pwd)
-SUBMODULE_PATH="packages/web-target/src"
+SUBMODULE_PATH="packages/web-target"
 LINK_PATH="$CURRENT_DIR/packages/app/src" 
 REPO_URL="git@github.com:yongheng2016/src-bex.git"
-SUBMODULE_PATH="$CURRENT_DIR/$SUBMODULE_PATH" 
+LINK_SOURCE="$CURRENT_DIR/$SUBMODULE_PATH/src" 
 
 if git config --file .gitmodules --get-regexp "submodule\.$SUBMODULE_PATH\.url" | grep -q "$REPO_URL"; then
     echo "The submodule already exists, skip adding it."
@@ -15,11 +15,11 @@ else
     echo "The submodule is added."
 fi
 
-if [ -d "$SUBMODULE_PATH" ]; then
+if [ -d "$LINK_SOURCE" ]; then
     while true; do
         if [ ! -L "$LINK_PATH" ]; then
-            ln -s "$SUBMODULE_PATH" "$LINK_PATH"
-            echo "A soft connection is created: $LINK_PATH -> $SUBMODULE_PATH"
+            ln -s "$LINK_SOURCE" "$LINK_PATH"
+            echo "A soft connection is created: $LINK_PATH -> $LINK_SOURCE"
             break 
         else
             echo "The soft connection already exists: $LINK_PATH, skip creating it."
@@ -28,5 +28,5 @@ if [ -d "$SUBMODULE_PATH" ]; then
         sleep 2 
     done
 else
-    echo "Error: submodule path does not exist: $SUBMODULE_PATH"
+    echo "Error: submodule path does not exist: $LINK_SOURCE"
 fi
